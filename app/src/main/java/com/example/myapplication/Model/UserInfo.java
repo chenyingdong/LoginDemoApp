cffa75f5ce65a04a19f1327eb02a426c9c240e8f
chenyingdong <1185605733@qq.com>
2019-05-08 23:18:52 +0800

    First Commit

---
+++ app/src/main/java/com/example/myapplication/Model/UserInfo.java
@@ -0,0 +1,11 @@
+package com.example.myapplication.Model;
+
+public class UserInfo {
+    public String username;
+    public String password;
+
+    public UserInfo(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
